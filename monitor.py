83e457dcee97974dc469a8a1d8a5272d091e4193
mrobinson <mrobinson7627@gmail.com>
2012-11-23 16:36:49 -0500

    Cleaned up code, still broken

--- monitor.py
+++ monitor.py
@@ -6,7 +6,6 @@ from deluge.error import InvalidTorrentError
 
 
 class Monitor(object):
-
     def __init__(self):
         self.trackerlist = {"animebyt.es", "bakabt.me",
                             "bitgamer.su", "speed.cd", "what.cd"}
@@ -16,11 +15,9 @@ class Monitor(object):
         self.torrentlist.append(torrent_id)
 
     def cleanTorrents(self):
-        if self.torrentlist:
-            for i in self.torrentlist:
-                torrent_id = i
-                status = client.core.get_torrent_status(torrent_id, ['tracker_host', 'ratio', 'active_time', 'progress'])
-                status.addCallback(cleanUp, torrent_id)
+        for torrent_id in self.torrentlist:
+            status = client.core.get_torrent_status(torrent_id, ['tracker_host', 'ratio', 'active_time', 'progress'])
+            status.addCallback(cleanUp, torrent_id)
 
 
 def cleanUp(status, torrent_id):
@@ -31,8 +28,6 @@ def cleanUp(status, torrent_id):
             privatetracker = True
             break
 
-    #if ((status['progress'] == 1.0 and
-    #        status['ratio'] >= 1.0 and not privatetracker) or (status['progress'] == 1.0 and status['active_time'] == 10 and not privatetracker)):
     if status['progress'] == 1:
         try:
             client.core.remove_torrent(torrent_id,
@@ -44,9 +39,7 @@ def cleanUp(status, torrent_id):
 
 def checkdirectory(directory):
     contents = os.listdir(directory)
-    i = 0
-    while i < len(contents):
-        if string.find(contents[i], ".torrent") > 0:
-            return contents[i]
-        i += 1
+    for i in contents:
+        if string.find(i, ".torrent") > 0:
+            return i
     return ""
